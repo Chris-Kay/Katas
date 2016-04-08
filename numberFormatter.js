5050854d0aebf4a7542b05c6f3db0d8411492b5d
Chris-Kay <chris.kay01@bbc.co.uk>
2016-04-08 10:53:08 +0100

    Finished number formatter

--- numberFormatter.js
+++ numberFormatter.js
@@ -1,58 +1,73 @@
-var formattedSeconds,
-    formattedMins,
-    formattedHours;
+var times;
 
-function formatFinalNumber(formattedSeconds, formattedMins, formattedHours) {
-    formattedSeconds = formattedSeconds === "0 seconds" ? undefined : formattedSeconds;
-    formattedMins = formattedMins === "0 mins" ? undefined : formattedMins;
-    formattedHours = formattedHours === "0 hours" ? undefined : formattedHours;
-
-
-    if(formattedSeconds && formattedMins === undefined) {
-        return formattedSeconds;
-    } else if (formattedSeconds === undefined && formattedHours == undefined && formattedMins) {
-        return formattedMins;
-    } else if(formattedSeconds && formattedMins && formattedHours === undefined){
-        return formattedMins + " and " + formattedSeconds;
-    } else if(formattedHours && formattedMins && formattedSeconds) {
-        return formattedHours + ", " + formattedMins + " and " + formattedSeconds;
-    } else {
-        return formattedHours;
-    }
-}
 function formatDuration (seconds) {
 // Complete this function
+times = [];
 
     if(seconds === 0) {
         return 'now';
-    } else if(seconds < 60) {
-        formattedSeconds = formatSeconds(seconds);
-    } else if (seconds < 3600) {
-        formattedMins = formatMins(seconds);
-    } else {
-        formattedHours = formatHours(seconds);
     }
 
-    return formatFinalNumber(formattedSeconds, formattedMins, formattedHours);
+    var delta = seconds;
+    var days = Math.floor(delta / 86400);
+    if(days > 0) {
+        formatDays(days);
+    }
+    delta -= days * 86400;
+
+    // calculate (and subtract) whole hours
+    var hours = Math.floor(delta / 3600) % 24;
+    if(hours > 0) {
+        formatHours(hours);
+    }
+    delta -= hours * 3600;
+
+    // calculate (and subtract) whole minutes
+    var minutes = Math.floor(delta / 60) % 60;
+    if(minutes > 0) {
+        formatMins(minutes);
+    }
+    delta -= minutes * 60;
+
+    // what's left is seconds
+    var seconds = delta % 60; 
+    if(seconds > 0) {
+        formatSeconds(seconds);
+    } // in theory the modulus is not required
+
+    return formatArray(times);
+}
+
+function formatArray(arr){
+    var outStr = "";
+    if (arr.length === 1) {
+        outStr = arr[0];
+    } else if (arr.length === 2) {
+        outStr = arr.join(' and ');
+    } else if (arr.length > 2) {
+        outStr = arr.slice(0, -1).join(', ') + ' and ' + arr.slice(-1);
+    }
+    return outStr;
 }
 
 function formatSeconds(seconds) {
     var secondString = seconds === 1 ? 'second' : 'seconds';
-    return seconds.toString() + ' ' + secondString;
+    times.push(seconds.toString() + ' ' + secondString);
 }
 
-function formatMins(seconds) {
-    var mins = Math.floor((seconds / 60));
+function formatMins(mins) {
     var minsString = mins === 1 ? 'minute' : 'minutes';
-    formattedSeconds = formatSeconds(seconds - mins * 60);
-    return mins.toString() + ' ' + minsString;
+    times.push(mins.toString() + ' ' + minsString);
 }
 
-function formatHours(seconds) {
-    var hours = Math.floor(((seconds / 60) / 60));
+function formatHours(hours) {
     var hourString = hours === 1 ? 'hour' : 'hours';
-    formattedMins = formatMins(seconds - ((hours * 60) * 60));
-    return hours.toString() + " " + hourString;
+    times.push(hours.toString() + " " + hourString);
+}
+
+function formatDays(days) {
+    var dayString = days === 1 ? 'day' : 'days';
+    times.push(days.toString() + " " + dayString);
 }
 
 assertEquals(formatDuration(1), "1 second");
